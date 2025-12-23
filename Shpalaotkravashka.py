8867481d15fa69dd92379057c62ada683e98cb7f
kisij <kisij8607@gmail.com>
2025-12-23 14:59:49 +0200

    Archiver with progress bar (work in progress)

--- Shpalaotkravashka.py
+++ Shpalaotkravashka.py
@@ -30,7 +30,27 @@ tk.Button(root, text="архивируй свой пакет", command=open_file
 fileukaz = tk.Label(root, text="Файл не выбран")
 fileukaz.pack()
 def zakryvashka():
-    with zipfile.ZipFile(selectedfile + ".zip", 'w') as zipf:
-        zipf.write(selectedfile)
+    if not selectedfile:
+        messagebox.showerror('Ошибка долбаеба', 'Ебанутый?Выбери файл')
+        return
+    try:
+        print("архивация началась")
+        sostoyanie['value'] = 0
+        root.update()
+        with zipfile.ZipFile(selectedfile + ".zip", 'w') as zipf:
+            #50%
+            sostoyanie['value'] = 50
+            root.update()
+            zipf.write(selectedfile)
+            sostoyanie['value'] = 100
+            root.update()
+            messagebox.showinfo("Ура получилось,", f"Архив создан!\n{selectedfile}.zip")
+            print(f"создан архив {selectedfile}.zip")
+    except Exception as e:
+        messagebox.showerror("Ошибка", f"Не удалось:\n{str(e)}")
+        sostoyanie['value'] = 0
 tk.Button(root, text="НАЧАТЬ", command=zakryvashka).pack(pady=30)
+sostoyanie = ttk.Progressbar(root, length=300, mode="determinate")
+sostoyanie.pack(pady=30)
+sostoyanie['value'] = 0
 root.mainloop()
